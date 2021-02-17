d0455ab6b63bffa7d0d58b9f1bc9de476df33aaf
20185561 <20185561@DSN1271158622.cfp127aluno.local>
2021-02-17 15:22:14 -0200

    first project

---
+++ semaforoMAQest.X/main.c
@@ -0,0 +1,18 @@
+/*
+ * File:   main.c
+ * Author: 20185561
+ *
+ * Created on 17 de Fevereiro de 2021, 15:12
+ */
+
+
+#include <xc.h>
+#include "config.h"
+
+void main(void) 
+{
+    while ( 1 )
+    {
+        
+    }
+}
